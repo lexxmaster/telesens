3fb1195ef5addb5044bbbb430a055ec62bc0403e
lexxmaster <alexeykupriy@gmail.com>
2022-07-04 00:18:13 +0300

    solve 3.3

---
+++ Task_3_3/src/CalcPow8.java
@@ -0,0 +1,13 @@
+public class CalcPow8 {
+    public static int[] CalculatePow8(int n){
+        if(n <= 0) return new int[0];
+
+        int[] arr = new int[n];
+        arr[0] = 8;
+        for (int i = 1; i < n; i++){
+            arr[i] = arr[i-1] * 8;
+        }
+
+        return arr;
+    }
+}
---
+++ Task_3_3/src/Test_3_3.java
@@ -0,0 +1,9 @@
+public class Test_3_3 {
+    public static void main(String[] args) {
+        int n = 6;
+        int[] arr = CalcPow8.CalculatePow8(n);
+        for(int i = 0; i < n; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
